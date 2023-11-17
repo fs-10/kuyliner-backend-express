685a048f7ff484ab0590ea98327b4b8afd308272
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-11-17 11:39:10 +0700

    fix: tidyng up code models

--- models/Product.model.js
+++ models/Product.model.js
@@ -1,23 +1,23 @@
-  const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-  const product_schema = new mongoose.Schema({
-    name_product: { type: String, required: true },
-    price: { type: Number, required: true },
-    product_image: String,
-    supplier_id: {
-      type: mongoose.ObjectId,
-      ref: "Supplier",
-    },
-    user_id: {
-      type: mongoose.ObjectId,
-      ref: "User",
-    },
-    category_id: {
-      type: mongoose.ObjectId,
-      ref: "Category",
-    },
-  });
+const product_schema = new mongoose.Schema({
+  name_product: { type: String, required: true },
+  price: { type: Number, required: true },
+  product_image: String,
+  supplier_id: {
+    type: mongoose.ObjectId,
+    ref: "Supplier",
+  },
+  user_id: {
+    type: mongoose.ObjectId,
+    ref: "User",
+  },
+  category_id: {
+    type: mongoose.ObjectId,
+    ref: "Category",
+  },
+});
 
-  const Product = mongoose.model("Product", product_schema);
+const Product = mongoose.model("Product", product_schema);
 
-  module.exports = Product;
+module.exports = Product;
--- models/Review.model.js
+++ models/Review.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const review_schema = new mongoose.Schema({
   text_review: { type: String, required: true },
   stars: { type: Number, required: true },
-  date_create: {type: Date, required: true, default: Date.now},
+  date_create: { type: Date, required: true, default: Date.now },
   id_reviewers: {
     type: mongoose.ObjectId,
     ref: "User",
