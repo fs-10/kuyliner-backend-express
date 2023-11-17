313c8039b949ecde3aad0e63b49e087db2c77d39
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-11-17 22:42:55 +0700

    fix: fixing attributes to make kredibility

--- models/Suppliers.model.js
+++ models/Suppliers.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const suppliers_schema = new mongoose.Schema({
-  supplier_name: { type: String, required: true },
+  supplier_username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   first_name: { type: String, required: true },
@@ -11,8 +11,7 @@ const suppliers_schema = new mongoose.Schema({
   profile_image: String,
   open_time: Date,
   closed_time: Date,
-  open_day: String,
-  closed_day: String,
+  day_of_week: String,
   role_id: {
     type: mongoose.ObjectId,
     ref: "Role_Base",
