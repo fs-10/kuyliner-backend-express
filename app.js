554d2ce7d7769fa1fcff083254ca616157f8b9c2
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-11-17 08:15:19 +0700

    fix: add starter route code

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const allRoutes = require("./routes");
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(allRoutes);
+
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}\nRunning on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
