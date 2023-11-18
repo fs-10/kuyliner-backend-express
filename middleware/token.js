2682f4850769ddb02ba214867b07980e02eb7cb0
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-11-18 21:20:13 +0700

    feat: features token for authenctication

--- middleware/token.js
+++ middleware/token.js
@@ -4,21 +4,30 @@ const jwt = require("jsonwebtoken");
 const token = (req, res, next) => {
   try {
     const header = req.headers.authorization;
-    if (!header) throw new Error("Invalid header");
+
+    if (!header) {
+      res.status(401).json({
+        message: "Invalid header",
+      });
+    }
 
     const token = header.split(" ")[1];
-    if(!token) throw new Error("There is no token");
+
+    if (!token) {
+      res.status(404).json({
+        message: "There is no token",
+      });
+    }
 
     const verify_token = jwt.verify(token, process.env.KEY);
     req.verify_token = verify_token;
 
     next();
-
   } catch (error) {
     res.status(400).json({
       message: `Error : ${error.message}`,
-      token
     });
   }
-
 };
+
+module.exports = token;
