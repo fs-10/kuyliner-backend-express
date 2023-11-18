dfd25a38feb017b9c2e8084aaa623464166b06ad
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-11-18 23:41:29 +0700

    fix: fixing code on auth and index

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const auth = require('./auth.route');
-const role_base = require('./role.route');
+const auth = require("./auth.route");
+const role_base = require("./role.route");
+const review = require("./review.route");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   res.status(200).json({
-    message: "Welcome to kuyliner apps"
+    message: "Welcome to kuyliner apps",
   });
 });
 
-router.use(auth);
-router.use(role_base);
+router.use("/auth", auth);
+router.use("/role", role_base);
+
+// User
+router.use("/review", review);
+
+// Supplier
 
 module.exports = router;
