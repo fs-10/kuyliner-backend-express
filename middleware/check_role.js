e214b7fe5ecec9df6f84e41c8ea19964efeb5403
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-11-18 21:16:13 +0700

    fix: adding check role for authentication features

---
+++ middleware/check_role.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+function has_role(roles) {
+  return (req, res, next) => {
+    try {
+      const { authorization } = req.headers;
+      const token = authorization.split(" ")[1];
+      const { role } = jwt.decode(token);
+
+      if (roles !== role) {
+        res.status(401).json({
+          message: "Can't access this features",
+        });
+      } else {
+        res.status(200).json({
+          message: "Access success",
+        });
+
+        next();
+      }
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      res.status(400).json({
+        messsage: `Error: ${error.message}`,
+      });
+    }
+  };
+}
+
+module.exports = has_role;
