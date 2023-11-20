528df445ce3eda3fcc03835bc8ace900c5346671
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-11-21 06:39:25 +0700

    fix: resolving code

--- controllers/supplier_controller/auth.supplier.controller.js
+++ controllers/supplier_controller/auth.supplier.controller.js
@@ -49,14 +49,8 @@ module.exports = {
   },
   sign_up: async (req, res) => {
     try {
-      const {
-        supplier_username,
-        email,
-        password,
-        first_name,
-        address,
-        role_id,
-      } = req.body;
+      const { supplier_username, email, password, first_name, address } =
+        req.body;
 
       let {
         last_name,
@@ -67,35 +61,45 @@ module.exports = {
         day_of_week,
       } = req.body;
 
-      const saltRounds = 10;
-      const encrypt_password = bcrypt.hashSync(password, saltRounds);
+      const find_supplier = await Suppliers.findOne({ email: email });
 
-      last_name = last_name || "";
-      location_gmaps = location_gmaps || "";
-      profile_image = profile_image || "";
-      open_time = open_time || "";
-      closed_time = closed_time || "";
-      day_of_week = day_of_week || "";
+      if (find_supplier != null) {
+        if (find_supplier.email === email) {
+          res.status(400).json({
+            message: "Email already in use",
+          });
+        }
+      } else {
+        const saltRounds = 10;
+        const encrypt_password = bcrypt.hashSync(password, saltRounds);
 
-      const supplier = await Suppliers.create({
-        supplier_username,
-        email,
-        password: encrypt_password,
-        first_name,
-        last_name,
-        address,
-        location_gmaps,
-        profile_image,
-        open_time,
-        closed_time,
-        day_of_week,
-        role_id: "65576c984615aad8dc605014",
-      });
+        last_name = last_name || "";
+        location_gmaps = location_gmaps || "";
+        profile_image = profile_image || "";
+        open_time = open_time || "";
+        closed_time = closed_time || "";
+        day_of_week = day_of_week || "";
 
-      res.status(201).json({
-        message: "Account supplier has created",
-        any_message: `Create: ${supplier.supplier_username}`,
-      });
+        const supplier = await Suppliers.create({
+          supplier_username,
+          email,
+          password: encrypt_password,
+          first_name,
+          last_name,
+          address,
+          location_gmaps,
+          profile_image,
+          open_time,
+          closed_time,
+          day_of_week,
+          role_id: "65576c984615aad8dc605014",
+        });
+
+        res.status(201).json({
+          message: "Account supplier has created",
+          any_message: `Create: ${supplier.supplier_username}`,
+        });
+      }
     } catch (error) {
       console.log(`\nError : ${error}`);
       res.status(400).json({
