6c9bff8ba70d9f09b9e357712488879da2380935
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-11-20 20:33:16 +0700

    fix: adding scheme table Promotions

---
+++ models/Promotions.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const promotions_schema = new mongoose.Schema({
+  due_date: { type: Date, required: true },
+  start_date: { type: Date, required: true, default: Date.now() },
+  status: { type: Boolean, required: true, default: false },
+  product_id: {
+    type: mongoose.ObjectId,
+    ref: "Products",
+  },
+});
+
+const Promotions = mongoose.model("Promotions", promotions_schema);
+
+module.exports = Promotions;
