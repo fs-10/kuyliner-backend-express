6fafe77c8a967582625a3532ff5e26a1bd76b6af
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-11-23 18:35:59 +0700

    fix: adding token on account user route

--- routes/user_route/account.user.route.js
+++ routes/user_route/account.user.route.js
@@ -7,9 +7,11 @@ const {
 } = require("../../controllers/user_controller/account.user.controller");
 const router = express.Router();
 
+const { token } = require("../../middleware");
+
 // router.get("/", getAllAccount);
 router.get("/:userId", getAccountById);
-router.get("/:userId", editAccount);
-router.get("/:userId", deleteAccount);
+router.get("/:userId", token, editAccount);
+router.get("/:userId", token, deleteAccount);
 
 module.exports = router;
